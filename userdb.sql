11f1b5022a96be6568419eaca0ee17e9d605448f
Amna Sadiq <59893281+amnasadiq0073@users.noreply.github.com>
2021-06-02 23:02:54 +0500

    Add files via upload

---
+++ userdb.sql
@@ -0,0 +1,13 @@
+create database userdb;
+
+use userdb
+
+CREATE TABLE loginuser(
+username varchar(255) NOT NULL PRIMARY KEY,
+password varchar(255) NOT NULL
+);
+
+
+
+
+insert into loginuser values('amna','amna')
